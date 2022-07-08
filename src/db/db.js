f517f50929443b7f09d4e8d51882dec795b9034b
HardikSharmaone8 <hardiksharma30018@gmail.com>
2022-07-08 14:30:54 +0530

    err msg

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose
     .connect(
-        process.env.MONGODB_URL ||
-        "mongodb+srv://hardik_sharma_one8:Om$hiv12@newcluster.3tmykg1.mongodb.net/?retryWrites=true&w=majority" ||
-        "mongodb://localhost:27017/donordatabase", {
+        "mongodb+srv://hardik_sharma_one8:Om$hiv12@newcluster.3tmykg1.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }
